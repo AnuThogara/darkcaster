c3460904043d2a457d6ede23f7ba8bf51c62bfeb
AnuThogara <anuradha.thogara@gmail.com>
2016-10-18 19:02:09 -0400

    refactor forecast to seperate router

--- index.js
+++ index.js
@@ -2,33 +2,22 @@ var express = require('express');
 var cors = require('cors');
 var server = express();
 var logger = require('./middleware/logger.js');
-var axios = require('axios');
-var apikey = require('./config.js').apikey;
+
+var forecastRouter = require('./router/forecast.router.js');
 
 var port = process.env.PORT || 8080;
-
+//middleware
 server.use(express.static(__dirname+'/public'));
 server.use(logger);
 server.use(cors());
+
+
 server.get('/', function(request,response){
   response.sendFile('public/html/index.html',{root:__dirname});
 });
 
-server.get('/forecast/:latitude,:longitude', function(request, response){
-  // response.send("Latitude: " + request.params.latitude + " Longitude: " + request.params.longitude);
-  var url ='https://api.darksky.net/forecast/'+ apikey +'/'+ request.params.latitude +','+ request.params.longitude;//url from darksky
-  var timeoutConfig ={
-  timeout: 2000
-};
-  axios.get(url, timeoutConfig)
+server.use(forecastRouter);
 
-  .then(function(forecast){
-    response.send(forecast.data);
-  })//when it's successful
-  .catch(function(error){
-    response.send(error);
-  });
-});
 server.listen(port, function(){
   console.log('now listening on port ..', port);
 });
---
+++ router/forecast.router.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var apikey = require('../config.js').apikey;
+var axios = require('axios');
+var timeoutConfig = {
+    timeout: 2000
+};
+
+router.get('/forecast/:latitude,:longitude', function(request, response) {
+    var url = buildForecastURL(request.params.latitude, request.params.longitude);
+
+    axios.get(url, timeoutConfig) //get to the url
+
+    .then(function(forecast) {
+            response.send(forecast.data);
+        }) //when it's successful
+        .catch(function(error) {
+            response.send(error);
+        });
+});
+
+module.exports = router;
+
+function buildForecastURL(latitude, longitude) {
+
+    var url = 'https://api.darksky.net/forecast/' + apikey + '/' + latitude + ',' + longitude; //url from darksky, build url and spit to diff url
+    return url;
+}
+
+
+
+// server.get('/forecast/:latitude,:longitude', function(request, response){
+//   // response.send("Latitude: " + request.params.latitude + " Longitude: " + request.params.longitude);
+
+// });
