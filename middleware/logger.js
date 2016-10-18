9ecde636fa1ed4643cc8f51304361806cc6cf1a4
AnuThogara <anuradha.thogara@gmail.com>
2016-10-17 20:50:07 -0400

    configured apikey, latitude and longitude

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,6 +2,7 @@ function logger(request, response, next){
   var requestDate = new Date();
   var requestMethod = request.method;
   var requestUrl = request.url;
+
   console.log(requestDate, requestMethod, requestUrl);
   next();
 }
